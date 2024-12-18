a1bef878c0767debe55332f77edc925c0f1c4242
muraterennar <muraterennar@gmail.com>
2024-12-18 23:37:40 +0300

    [fix] fixed like service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import { MERMAID_OPTIONS, provideMarkdown} from 'ngx-markdown';
 import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(),
+    provideAnimations(),
     provideMarkdown({
       mermaidOptions: {
         provide: MERMAID_OPTIONS,
--- src/app/services/like.service.ts
+++ src/app/services/like.service.ts
@@ -1,24 +1,26 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeService {
-  uri: string = 'https://angular-text-editor-api.onrender.com/api/likes';
+  private apiUrl = 'https://angular-text-editor-api.onrender.com/api/likes'; // API URL
 
   constructor(private httpClient: HttpClient) {
   }
 
-  getLikes() {
-    return this.httpClient.get(`${this.uri}`);
+  // Like verisini alır
+  getLikes(): Observable<{ likes: number }> {
+    return this.httpClient.get<{ likes: number }>(this.apiUrl);
   }
 
-  likePost() {
-    return this.httpClient.post(`${this.uri}/inc`, {});
+  likePost(): Observable<{ likes: number }> {
+    return this.httpClient.post<{ likes: number }>(`${this.apiUrl}/inc`, {});
   }
 
-  resetLikes() {
-    return this.httpClient.post(`${this.uri}/reset`, {});
+  resetLikes(): Observable<{ likes: number }> {
+    return this.httpClient.post<{ likes: number }>(`${this.apiUrl}/reset`, {});
   }
 }
